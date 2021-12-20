0ce9b8f7b416481a7c58e5ee2c034c4b3a902514
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2021-12-19 23:39:16 -0300

    dificulty sistem

--- src/script.js
+++ src/script.js
@@ -1,74 +1,118 @@
-const main = document.querySelector("main")
-let names = ["😃 ", "😄 ", "😁 ", "😆 ", "😅 ", "😂 ", "🤣  ", "😊 ", "😇 ", "🙂 ", "🙃 ", "😉 ", "😍 ", "🥰 ", "😘 ", "😗 ", "😙 ", "😝 "]
-let countOnclick = 0
-let countSucsess = 0
-let isRunning=false
+const main = document.querySelector("main");
+let names = [
+  "😃 ",
+  "😄 ",
+  "😁 ",
+  "😆 ",
+  "😅 ",
+  "😂 ",
+  "🤣  ",
+  "😊 ",
+  "😇 ",
+  "🙂 ",
+  "🙃 ",
+  "😉 ",
+  "😍 ",
+  "🥰 ",
+  "😘 ",
+  "😗 ",
+  "😙 ",
+  "😝 ",
+];
+let countOnclick = 0;
+let countSuccess = 0;
+let isRunning = false;
+const dificulty = {
+  facil: 8,
+  normal: 12,
+  dificil: 18,
+};
 
 const resetCard = (element) => {
-    countOnclick = 0
-    element.classList.remove("cardOnclick")
-}
+  countOnclick = 0;
+  element.classList.remove("cardOnclick");
+};
 
+const starting = document.querySelector("#starting");
+const header = document.querySelector("header");
+
+starting.addEventListener("click", () => {
+  let dificultyChosen;
+  const dificultyInput = document.getElementsByName("dificuldade");
+
+  dificultyInput.forEach((input) => {
+    if (input.checked) {
+      dificultyChosen = dificulty[input.value];
+    }
+  });
+  createElements(dificultyChosen);
+  header.style.display = "none";
+  main.style.height = "auto";
+});
 
 const validateValues = (event) => {
-    //*Fluxo de validação dos valores
-    if (countOnclick == 2 && event.target.classList.contains("cardOnclick")) {
-        let elementsActive = document.querySelectorAll(".cardOnclick")
-        let notSucsess = []
+  //*Fluxo de validação dos valores
+  if (countOnclick == 2 && event.target.classList.contains("cardOnclick")) {
+    let elementsActive = document.querySelectorAll(".cardOnclick");
+    let notSucsess = [];
 
-        elementsActive.forEach(card => {
-            if (!card.classList.contains("sucess")) {
-                notSucsess.push(card)
-            }
-        })
-        //?verificar se os valores sao iguais
-setTimeout(()=>{  
-    if (notSucsess[0].innerHTML === notSucsess[1].innerHTML) {
-        elementsActive.forEach(e => { e.classList.add("sucess") })
-        countSucsess++
+    elementsActive.forEach((card) => {
+      if (!card.classList.contains("sucess")) {
+        notSucsess.push(card);
+      }
+    });
+    //?verificar se os valores sao iguais
+    setTimeout(() => {
+      if (notSucsess[0].innerHTML === notSucsess[1].innerHTML) {
+        elementsActive.forEach((e) => {
+          e.classList.add("sucess");
+        });
+        countSuccess++;
+      }
+      notSucsess.forEach((element) => resetCard(element));
+      isRunning = false;
+    }, 600);
+  }
+  if (countOnclick < 2) {
+    setTimeout(() => {
+      isRunning = false;
+    }, 200);
+  }
+};
 
-    }
-    notSucsess.forEach(element => resetCard(element))
-    isRunning=false 
-},600)
-    }
-    if(countOnclick<2){
-        setTimeout(()=>{
-            isRunning=false 
-        },200)
-    }
-}
+const handleCard = (event, count) => {
+  //*Adicionando a classe de card ativo
+  if (!event.target.classList.contains("cardOnclick")) {
+    event.target.classList.add("cardOnclick");
+    countOnclick++;
+  }
+  validateValues(event);
+  //*Fluxo de sucesso
 
+  if (countSuccess + 1 == count && countOnclick == 2) {
+    alert("parabens");
+    main.style.height = "0";
+    header.style.display = "flex";
+  }
+};
 
-const handleCard = (event) => {
-    //*Adicionando a classe de card ativo 
-    if (!event.target.classList.contains("cardOnclick")) {
-        event.target.classList.add("cardOnclick")
-        countOnclick++
-    }
-    validateValues(event)
-    //*Fluxo de sucesso
-    if (countSucsess == 18 && countOnclick == 0) {
-        alert("parabens")
-    }
- 
-}
 // *criando cards
+const createElements = (quantityElements) => {
+  main.innerHTML = "";
+  for (let i = 0; i < quantityElements * 2; i++) {
+    let index = i < quantityElements ? i : i - quantityElements;
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = names[index];
+    card.style.order = Math.floor(Math.random() * i);
 
-for (let i = 0; i < 36; i++) {
-    let index = i < 18 ? i : i - 18
-    const card = document.createElement("div")
-    card.className = "card"
-    card.innerHTML = names[index]
-    card.style.order = Math.floor(Math.random() * i)
+    card.addEventListener("click", (e) => {
+      if (!e.target.classList.contains("sucess") && !isRunning) {
+        isRunning = true;
+        handleCard(e, quantityElements);
+      }
+    });
 
-    card.addEventListener("click", e => {
-        if (!e.target.classList.contains("sucess") && !isRunning) {
-            isRunning=true
-            handleCard(e)
-        }
-    })
-
-    main.appendChild(card)
-}
-
+    main.appendChild(card);
+  }
+};
